49ea9bd783ca4754ab3e91e7387dcae93f5e3d77
Paul Gibbs <pwgibbs10@gmail.com>
2017-12-03 22:20:19 -0700

    Added four pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def white
+  end
+
+  def red
+  end
+
+  def black
+  end
+
+  def pale
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/white'
+
+  get 'pages/red'
+
+  get 'pages/black'
+
+  get 'pages/pale'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
